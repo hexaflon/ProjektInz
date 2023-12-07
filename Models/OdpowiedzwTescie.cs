1139c1672d730d6233e41d7a2e0db48ee351e7f2
Hexaflon <hexilon3214@gmail.com>
2023-12-07 09:41:20 +0100

    Pages\Exam && Models Exam: A-ExamList(list of active exams visible for user) A-ExamPerform(Started, should show selected exam to solve by user) Models: A-OdpowiedziwTescie(Not sure yet)

---
+++ Models/OdpowiedzwTescie.cs
@@ -0,0 +1,17 @@
+﻿namespace ProjektInzynierski.Models
+{
+    public class OdpowiedzwTescie
+    {
+        public OdpowiedzwTescie()
+        {
+
+        }
+        public OdpowiedzwTescie(int idodpowiedz)
+        {
+            this.idOdpowiedz = idodpowiedz;
+        }
+
+        public int idOdpowiedz;
+
+    }
+}
---
+++ Pages/Exam/ExamPerform.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TestTest.Models.Db;
+
+namespace ProjektInzynierski.Pages.Exam
+{
+    public class ExamPerformModel : PageModel
+    {
+        private readonly TestTest.Models.Db.DatabaseContext _context;
+
+        public ExamPerformModel(TestTest.Models.Db.DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IList<RozwiazanieDoPytan> RozwiazanieDoPytan { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.RozwiazanieDoPytan != null)
+            {
+                RozwiazanieDoPytan = await _context.RozwiazanieDoPytan
+                .Include(r => r.IdOdpowiedzNavigation)
+                .Include(r => r.IdRozwiazanieNavigation).ToListAsync();
+            }
+            RozwiazanieDoPytan x = new RozwiazanieDoPytan();
+            
+        }
+    }
+}
