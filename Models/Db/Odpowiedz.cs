8c84e6225b591b86d0f80c4abc352b56adfb43b6
s95367 <s95367@pollub.edu.pl>
2023-12-14 01:08:00 +0100

    U - Models/Db/Odpowiedz && Pages/Exam/Delete && Pages/Exam/Edit (validation & appearence)

--- Models/Db/Odpowiedz.cs
+++ Models/Db/Odpowiedz.cs
@@ -15,6 +15,7 @@ namespace TestTest.Models.Db
         public int IdOdpowiedz { get; set; }
         public int? IdPytanie { get; set; }
         [Display(Name = "Treść odpowiedzi")]
+        [Required(ErrorMessage = "To pole jest wymagane.")]
         [StringLength(255, ErrorMessage = "Maksymalna długość to 255 znaków.")]
         public string TrescOdpowiedzi { get; set; } = null!;
         [Display(Name = "Czy odpowiedź jest poprawna?")]
